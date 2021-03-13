7937f28630d4c4a6a8857b12955393ccd5e33fb9
Gioele Pannetto <gioele@pannetto.com>
2021-03-13 19:15:56 +0100

    User and StudentsClass

---
+++ backend/src/models/Class.ts
@@ -0,0 +1,12 @@
+export default class StudentsClass {
+    public id: number;
+    public className: string;
+
+    constructor(
+        id: number,
+        className: string,
+    ) {
+        this.id = id;
+        this.className = className;
+    }
+}
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,12 +1,28 @@
+import StudentsClass from "./Class";
+
 export enum UserType {
     student,
     prof
 }
 
 export default class User {
+
+    public firstName: string;
+    public lastName: string;
+    public type: UserType;
+    public userClass?: StudentsClass;
+
+
     constructor(
-        public firstName: string,
-        public lastName: string,
-        public type: UserType,
-    ) {}
+        firstName: string,
+        lastName: string,
+        type: UserType,
+        userClass?: StudentsClass,
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.type = type;
+        this.userClass = userClass;
+    }
+
 }
