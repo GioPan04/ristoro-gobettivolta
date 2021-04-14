0166bbc826bc0cdfa0f5f2cbcbb2266a9a8f01db
Gioele Pannetto <gioele@pannetto.com>
2021-04-15 01:03:29 +0200

    Console error when 500

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -17,6 +17,7 @@ router.all('*', (req, res) => {
 });
 
 router.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
     return res.status(500).json({ error: true, statusCode: 500 });
 });
 
